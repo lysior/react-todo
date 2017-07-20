fbb28a3e5cc729b702b228203ac54d5add95ecda
Stratmann <JStratma@sport1.de>
2017-07-20 18:22:44 +0200

    Main App component TodoApp

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Router, Route, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 //Load foundation-sites
 
 $(document).foundation();
@@ -9,6 +11,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <p>Boilerplate3</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
